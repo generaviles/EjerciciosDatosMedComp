d912b34d6e56ddca37c0889d549c79d667bd9b75
Gener Aviles R <generaviles@Geners-MacBook-Air.local>
2023-04-21 12:21:10 -0700

    EDA avances

--- EDA_ejercicio.R
+++ EDA_ejercicio.R
@@ -16,9 +16,55 @@ merge_planes <- merge(merge_airlines, planes, by = "tailnum", all.x = TRUE, suff
 merge_airports_origin <- merge(merge_planes, airports, by.x = "origin", by.y = "faa", all.x = TRUE, suffixes = c("_carrier", "_origin"))
 final_data <- merge(merge_airports_origin, airports, by.x = "dest", by.y = "faa", all.x = TRUE, suffixes = c("_origin", "_dest"))
 
+## Segmentando la base de datos para quedarnos con un subconjunto de práctica
 
 #############################################################
 #############################################################
 ## Evaluación de Calidad de los Datos
 #############################################################
 #############################################################
+library(dlookr)
+library(kableExtra)
+library(dplyr)
+
+## Usando DlookR
+final_data %>%
+  diagnose() %>%
+  kbl() %>%
+  kable_paper("hover", full_width = F)
+
+######## Recategorización de variables ######
+
+### MES ###
+final_data %>%
+  select(month) %>%
+  as.vector() %>%
+  as.factor() %>%
+  recode_factor(1 = "Enero")
+  head()
+
+
+final_data$month <- as.factor(final_data$month)
+levels(final_data$month) <- c("Enero",
+                                 "Febrero",
+                                 "Marzo",
+                                 "Abril",
+                                 "Mayo",
+                                 "Junio",
+                                 "Julio",
+                                 "Agosto",
+                                 "Septiembre",
+                                 "Octubre",
+                                 "Noviembre",
+                                 "Diciembre")
+str(final_data$month)
+
+table(final_data$month)
+
+## Tabla de conteo de mes.
+final_data %>%
+  select(month) %>%
+  table() %>%
+  kbl() %>%
+  kable_paper("hover", full_width = F)
+
